52f61524da0e5ee1ea7f4cf2e7b4bb23d9c41514
Kenton Varda <kenton@sandstorm.io>
2017-09-16 17:44:29 -0700

    Hard-code necessary compiler flags for my environment.
    Note that without -xc++, headers are interpreted as C by default.

--- clangd/GlobalCompilationDatabase.cpp
+++ clangd/GlobalCompilationDatabase.cpp
@@ -30,7 +30,7 @@ static void addExtraFlags(tooling::CompileCommand &Command,
 }
 
 tooling::CompileCommand getDefaultCompileCommand(PathRef File) {
-  std::vector<std::string> CommandLine{"clang", "-fsyntax-only", File.str()};
+  std::vector<std::string> CommandLine{"clang", "-fsyntax-only", "-xc++", "-std=c++17", "-I/home/kenton/src/all/src", "-I/home/kenton/src/all/tmp", File.str()};
   return tooling::CompileCommand(llvm::sys::path::parent_path(File),
                                  llvm::sys::path::filename(File), CommandLine,
                                  /*Output=*/"");
